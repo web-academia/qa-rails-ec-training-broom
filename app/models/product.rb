0f7672faf6b6404c4849f94710fd445793fb0d2b
Kenshiro Ando <kenshi@KenshironoAir.lan>
2023-06-25 19:38:14 +0900

    #8 dependentオプション追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
-    has_many :purchases
+    has_many :purchases, dependent: :destroy
+    # TODO: Order関係の構築ができたタイミングでコメントアウトを解除すること
+    # has_many :order_details, dependent: :destroy
+    # TODO: User関係の構築ができたタイミングでコメントアウトを解除すること
     # belongs_to :user
     belongs_to :category
     belongs_to :sale_status
