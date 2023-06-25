ee3a1f45c8e437361e6b8ad3b830dee235890162
Kenshiro Ando <kenshi@KenshironoAir.lan>
2023-06-25 22:29:49 +0900

    #8 インデント修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ApplicationRecord
-    has_many :purchases, dependent: :destroy
-    # TODO: Order関係の構築ができたタイミングでコメントアウトを解除すること
-    # has_many :order_details, dependent: :destroy
-    # TODO: User関係の構築ができたタイミングでコメントアウトを解除すること
-    # belongs_to :user
-    belongs_to :category
-    belongs_to :sale_status
-    belongs_to :product_status
+  has_many :purchases, dependent: :destroy
+  # TODO: Order関係の構築ができたタイミングでコメントアウトを解除すること
+  # has_many :order_details, dependent: :destroy
+  # TODO: User関係の構築ができたタイミングでコメントアウトを解除すること
+  # belongs_to :user
+  belongs_to :category
+  belongs_to :sale_status
+  belongs_to :product_status
 end
