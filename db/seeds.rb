9c6123b089cbd776f1996378b6eede62b8fd35e7
K-5963 <k.ando5963@gmail.com>
2023-07-09 08:14:35 +0900

    #17 データ投入 Seed Products関連

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create!(product_name: "電卓",
+                 category_id: 1,
+                 price: 250,
+                 description: "小型の電卓です",
+                 sale_status_id: 1,
+                 product_status_id: 1,
+                 user_id: 1)
+Product.create!(product_name: "ノート",
+                 category_id: 1,
+                 price: 150,
+                 description: "A4サイズのノートです",
+                 sale_status_id: 1,
+                 product_status_id: 1,
+                 user_id: 1)
+Product.create!(product_name: "鉛筆",
+                 category_id: 1,
+                 price: 80,
+                 description: "書きやすいです",
+                 sale_status_id: 1,
+                 product_status_id: 1,
+                 user_id: 2)
+Product.create!(product_name: "定規",
+                 category_id: 1,
+                 price: 100,
+                 description: "30cm定規です",
+                 sale_status_id: 1,
+                 product_status_id: 1,
+                 user_id: 2)
