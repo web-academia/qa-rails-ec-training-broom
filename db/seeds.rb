4657452ceca6ffe15ee3f0fcf28bb03a397b1e1a
K-5963 <k.ando5963@gmail.com>
2023-07-09 17:20:55 +0900

    #17 Product関連データ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+UserClassfication.create!(user_classfication_name: "一般ユーザー")
+
+User.create!(last_name: "田中", 
+             first_name: "A男", 
+             password: "hogehoge", 
+             zipcode: "1234567", 
+             email: "hoge1@example.com", 
+             phone_number: "09011112222", 
+             user_classfication_id: 1)
+
+User.create!(last_name: "鈴木", 
+             first_name: "B子", 
+             password: "hogehoge", 
+             zipcode: "1234567", 
+             email: "hoge2@example.com", 
+             phone_number: "09022223333", 
+             user_classfication_id: 1)
+
+Category.create!(category_name: "文具")
+SaleStatus.create!(sale_status_name: "閲覧済み")
+ProductStatus.create!(product_status_name: "新品")
+
 Product.create!(product_name: "電卓",
                  category_id: 1,
                  price: 250,
@@ -33,3 +56,5 @@ Product.create!(product_name: "定規",
                  sale_status_id: 1,
                  product_status_id: 1,
                  user_id: 2)
+
+Purchase.create!(product_id: 1, purchase_price: 500, purchase_quantity: 2, purchase_company: "A株式会社")
