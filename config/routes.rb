40885f276e11c04f5482afff129156506704d727
y-kida-Rails-study <yk-_-ao.rin@outlook.jp>
2023-07-21 10:56:56 +0900

    #29　ログイン画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
   resources :products
+  resources :users
+  get '/login', to: 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
