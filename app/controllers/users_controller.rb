4bce61c2283aa85c29f90084dfe54c092526708f
y-kida-Rails-study <yk-_-ao.rin@outlook.jp>
2023-07-27 22:23:34 +0900

    #28 users_controller.rbにupdateアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,22 @@ class UsersController < ApplicationController
   def edit
     @user = User.find_by(id: params[:id])
   end
-end
 
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.assign_attributes(user_params)
+    if @user.save
+      flash[:success] = '更新に成功しました'
+      redirect_to @user
+    else
+      flash.now[:danger] = '更新に失敗しました'
+      render "edit"
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:password, :last_name, :first_name, 
+    :zipcode, :prefecture, :municipality, :address, :apartments, :email, :phone_number, :user_classfication, :delete_flag)
+  end
+
+end
