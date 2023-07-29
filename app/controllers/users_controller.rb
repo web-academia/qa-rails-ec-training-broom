7cb6f17fe3c870e72e5a87a4a42dc021fb6b1d77
y-kida-Rails-study <yk-_-ao.rin@outlook.jp>
2023-07-29 23:18:34 +0900

    #28 rubocopを実行し、users_contoroller.rbの修正とja.ymlに追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,19 +11,17 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @user.assign_attributes(user_params)
     if @user.save
-      flash[:success] = '更新に成功しました'
+      flash[:success] = t user_update.success
       redirect_to @user
     else
-      flash.now[:danger] = '更新に失敗しました'
+      flash.now[:danger] = t user_update.failure
       render "edit"
     end
   end
 
   private
 
-  def user_params
-    params.require(:user).permit(:last_name, :first_name, 
-    :zipcode, :prefecture, :municipality, :address, :apartments, :email, :phone_number,)
-  end
-
+    def user_params
+      params.require(:user).permit(:last_name, :first_name, :zipcode, :prefecture, :municipality, :address, :apartments, :email, :phone_number)
+    end
 end
