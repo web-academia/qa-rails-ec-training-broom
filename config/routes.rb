764a83898db079e80d0907c2d86ec5f505901a91
y-kida-Rails-study <yk-_-ao.rin@outlook.jp>
2023-08-02 21:34:29 +0900

    #26 注文詳細画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :products
   resources :users
+  resources :orders
   get '/login', to: 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
