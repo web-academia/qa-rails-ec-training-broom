c716c700ec2af74cd75f76237977d40364063b17
K-5963 <k.ando5963@gmail.com>
2023-08-09 22:52:57 +0900

    #42 @productを削除

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,7 +3,6 @@ class CartItemsController < ApplicationController
 
   def create
     @cart = Cart.find_or_create_by!(user_id: current_user.id)
-    @product = Product.find_by(id: params[:product_id]) 
     if item = @cart.cart_items.find_by(product_id: params[:product_id], cart_id: @cart.id)
       item.quantity += params[:quantity].to_i
     else
