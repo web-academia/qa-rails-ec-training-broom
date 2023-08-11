5b9563bd91a5eaeb163870ff41dc5ed529cc2024
K-5963 <k.ando5963@gmail.com>
2023-08-11 21:37:33 +0900

    #42 orders#createトランザクション機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,16 +2,18 @@ class OrdersController < ApplicationController
   include SessionsHelper
 
   def create
-    @order = Order.create!(user_id: current_user.id, order_date: DateTime.now, order_number: rand(0o00000001..999_999_999))
-    cart_items = current_user.cart.cart_items
-    cart_items.each do |cart_item|
-      OrderDetail.create!(product_id: cart_item.product_id,
-                          order_id: @order.id,
-                          shipment_status_id: 1,
-                          order_quantity: cart_item.quantity,
-                          shipment_date: DateTime.now)
-    end
+    ActiveRecord::Base.transaction do
+      @order = Order.create!(user_id: current_user.id, order_date: DateTime.now, order_number: rand(0o00000001..999_999_999))
+      cart_items = current_user.cart.cart_items
+      cart_items.each do |cart_item|
+        OrderDetail.create!(product_id: cart_item.product_id,
+                            order_id: @order.id,
+                            shipment_status_id: 1,
+                            order_quantity: cart_item.quantity,
+                            shipment_date: DateTime.now)
+      end
     cart_items.destroy_all
-    render "purchase_completed"
+    render 'purchase_completed'
+    end
   end
 end
