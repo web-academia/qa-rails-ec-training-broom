58c3107ed7d139cddf7c032ad6a245ab14fa572a
K-5963 <k.ando5963@gmail.com>
2023-08-12 15:54:06 +0900

    #42 0から始まる数列にrubocopの指摘が入らないようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
 
   def create
     ActiveRecord::Base.transaction do
-      @order = Order.create!(user_id: current_user.id, order_date: DateTime.now, order_number: rand(0o00000001..999_999_999))
+      @order = Order.create!(user_id: current_user.id, order_date: DateTime.now, order_number: rand(000000001..999_999_999))
       cart_items = current_user.cart.cart_items
       cart_items.each do |cart_item|
         OrderDetail.create!(product_id: cart_item.product_id,
